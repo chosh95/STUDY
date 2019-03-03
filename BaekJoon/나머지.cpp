597a9d613e3aa9950307098e70178ecb90530af9
chosh95 <chosh-95@hanmail.net>
2019-03-03 10:05:07 +0900

    2019

--- BaekJoon/나머지.cpp
+++ BaekJoon/나머지.cpp
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
-
-=======
-/*
-#include <iostream>
-using namespace std;
->>>>>>> 9fe011e74c4cca81972d57c2f802553362fea3eb
 int main()
 {
 	int N,t=0,i=1;
@@ -28,7 +21,4 @@ int main()
 	if(N==0) cout<<i-1;
 	else if(t!=1) cout<<-1;
 }
-<<<<<<< HEAD
-=======
-*/
->>>>>>> 9fe011e74c4cca81972d57c2f802553362fea3eb
+
