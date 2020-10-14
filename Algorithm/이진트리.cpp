6733b9702aac9e2333a23cfa8ad9bc8c2a15121b
chosh95 <chosh-95@hanmail.net>
2020-10-14 20:56:21 +0900

    delete 함수 추가

--- Algorithm/이진트리.cpp
+++ Algorithm/이진트리.cpp
@@ -16,20 +16,29 @@ class Tree {
 	private:
 		Node* root;
 	public:
-		Tree(int data) {
-			Node* tmp = new Node(data);
-			this->root = tmp;
-		}
-		void traverse() { traverse(root); }
-		void add(int data) { add(root, data);}
+		Tree() { this->root = NULL; };
+		Tree(int data) { this->root = new Node(data);}
+
+		void traverse() { traverse(root); cout << endl; }
+		void add(int data);
+		void deleteNode(int data) { deleteNode(this->root, data);}
+
 		void traverse(Node* currentNode);
 		void add(Node* currentNode, int data);
+		Node* deleteNode(Node* currentNode, int data);
+		Node* findMax(Node* currentNode);
 };
+
 void Tree::traverse(Node* currentNode) {
 	if (currentNode == NULL) return;
 	cout << currentNode->data << " ";
 	traverse(currentNode->leftChild);
 	traverse(currentNode->rightChild);
+}
+
+void Tree::add(int data) {
+	if (this->root == NULL) root = new Node(data);
+	else add(this->root, data);
 }
 
 void Tree::add(Node* currentNode, int data) {
@@ -49,13 +58,53 @@ void Tree::add(Node* currentNode, int data) {
 			add(currentNode->leftChild, data);
 	}
 }
+
+Node* Tree::findMax(Node* currentNode) {
+	if (currentNode == NULL) return NULL;
+	while (currentNode->rightChild != NULL) {
+		currentNode = currentNode->rightChild;
+	}
+	return currentNode;
+}
+
+Node* Tree::deleteNode(Node* currentNode, int data) {
+	if (currentNode == NULL) return NULL;
+	if (data < currentNode->data) currentNode->leftChild = deleteNode(currentNode->leftChild, data);
+	else if (data > currentNode->data) currentNode->rightChild = deleteNode(currentNode->rightChild, data);
+	else{
+		if (currentNode->leftChild == NULL && currentNode->rightChild == NULL){
+			delete currentNode;
+			return NULL;
+		}
+		else if (currentNode->leftChild != NULL && currentNode->rightChild != NULL) {
+			Node* maxNode = findMax(currentNode->leftChild);
+			currentNode->data = maxNode->data;
+			currentNode->leftChild = deleteNode(currentNode->leftChild, maxNode->data);
+			return currentNode;
+		}
+		else {
+			Node* returnNode = currentNode->leftChild != NULL ? currentNode->leftChild : currentNode->rightChild;
+			delete currentNode;
+			return returnNode;
+		}
+	}
+	return currentNode;
+}
+
 int main()
 {
-	Tree t(3);
-	t.add(1);
-	t.add(0);
+	Tree t;
+	t.add(5);
+	t.add(3);
 	t.add(2);
-	t.add(6);
+	t.add(4);
+	t.add(8);
+	t.add(7);
+	t.add(9);
 	t.traverse();
 
+	t.deleteNode(5);
+	t.traverse();
+
+	
 }
